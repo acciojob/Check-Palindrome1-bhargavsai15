7aa2c08e3380d59891ae311496594b283d6313f4
bhargavsai15 <36848893+bhargavsai15@users.noreply.github.com>
2023-04-30 16:18:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
+	let low=0,high=str.length-1;
+	while(str.charAt(low)==str.charAt(high)){
+		low++;
+		high--;
+	}
 
+	if(low>high) return true;
+	else return false;
 }
 module.exports = palindrome
